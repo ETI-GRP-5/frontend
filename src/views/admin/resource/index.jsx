a106e03d42b8ec1e4b908da872c5c69beff32924
simon912 <s10228098@connect.np.edu.sg>
2024-02-03 22:56:10 +0800

    add delete button & modal for resource service frontend

--- src/views/admin/resource/index.jsx
+++ src/views/admin/resource/index.jsx
@@ -6,10 +6,12 @@ import { FaUpload } from "react-icons/fa";
 
 const ResourceUpload = () => {
     const [isModalOpen, setModalOpen] = useState(false);
+    const [isDeleteModalOpen, setDeleteModalOpen] = useState(false);
     const [file, setFile] = useState(null);
     const [isUploading, setIsUploading] = useState(false);
     const [fileUrl, setFileUrl] = useState('');
     const [localFilePath, setLocalFilePath] = useState('');
+    const [selectedResource, setSelectedResource] = useState(null);
     const [resources, setResources] = useState([]);
 
     useEffect(() => {
@@ -43,41 +45,64 @@ const ResourceUpload = () => {
             console.error('No files selected.');
             return;
         }
-
         setFile(selectedFiles[0]);
     };
 
     // close modal
     const closeModal = () => {
         setModalOpen(false);
+        setDeleteModalOpen(false);
+        setSelectedResource(null);
+    };
+
+    const openDeleteModal = (resource) => {
+        setSelectedResource(resource);
+        setDeleteModalOpen(true);
     };
 
     // css for modal
     const modalStyles = {
         content: {
-            width: '50%', // Set the width as per your requirement
-            height: '50%', // Set the height as per your requirement
-            margin: 'auto', // Center the modal
+            width: '50%',
+            height: '50%',
+            margin: 'auto',
         },
     };
     const handleDownload = (filePath) => {
         const downloadUrl = `http://localhost:3011/download?filePath=${encodeURIComponent(filePath)}`;
-
         // Create a hidden link element
         const link = document.createElement('a');
         link.href = downloadUrl;
         link.target = '_blank';
         link.download = filePath.split('/').pop(); // Set the downloaded file name
-
-        // Append the link to the body and trigger the download
         document.body.appendChild(link);
         link.click();
-
-        // Remove the link from the DOM
         document.body.removeChild(link);
     };
 
-    // for fetch 
+    const handleDelete = async () => {
+        if (selectedResource) {
+            try {
+                const response = await fetch(`http://localhost:3011/deleteresource?filePath=${encodeURIComponent(selectedResource.FilePath)}`, {
+                    method: 'DELETE',
+                });
+
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data.message);
+                    window.location.reload();
+                } else {
+                    console.error('Resource deletion failed');
+                }
+            } catch (error) {
+                console.error('Error deleting resource:', error.message);
+            } finally {
+                closeModal();
+            }
+        }
+    };
+
+    // fetch for upload to database and storage
     const uploadToDatabase = async () => {
         const formData = new FormData();
         formData.append('file', file);
@@ -117,32 +142,40 @@ const ResourceUpload = () => {
 
             }} onClick={handleUpload}>
                 Upload <FaUpload style={{ marginRight: '50px' }} /></button>
-            <div style={{ marginTop: '100px' }}>
-                <h1>Resource List</h1>
+            <div style={{ marginTop: '50px' }}>
                 {resources.length > 0 ? (
-                    <ul>
-                        {resources.map((resource, index) => (
-                            <li key={index}>
-                                <p>Name: {resource.Name}</p>
-                                <p>Path: {resource.FilePath}</p>
-                                <button onClick={() => handleDownload(resource.FilePath)}>
-                                    Download
-                                </button>
-                            </li>
-                        ))}
-                    </ul>
+                    <table style={{ width: '100%', borderCollapse: 'collapse', fontSize: '18px' }}>
+                        <thead>
+                            <tr style={{ borderBottom: '2px solid black', paddingBottom: '10px' }}>
+                                <th style={{ padding: '10px' }}>Name</th>
+                                <th style={{ padding: '10px' }}></th>
+                                <th style={{ padding: '10px' }}></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {resources.map((resource, index) => (
+                                <tr key={index} style={{ borderBottom: '1px solid #ddd', padding: '10px' }}>
+                                    <td>{resource.Name}</td>
+                                    <td>
+                                        <button onClick={() => handleDownload(resource.FilePath)}>
+                                            Download
+                                        </button>
+                                    </td>
+                                    <td>
+                                        <button onClick={() => openDeleteModal(resource)}>
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 ) : (
-                    <p>No resource has been uploaded.</p>
+                    <p>No resource has been uploaded...</p>
                 )}
-
             </div>
-            { }
-            <Modal
-                isOpen={isModalOpen}
-                onRequestClose={closeModal}
-                contentLabel="File Upload Successful"
-                style={modalStyles}
-            >
+
+            <Modal isOpen={isModalOpen} onRequestClose={closeModal} contentLabel="File Upload Successful" style={modalStyles}>
                 <h2 style={{
                     fontSize: '20px',
                     marginBottom: '20px'
@@ -159,6 +192,16 @@ const ResourceUpload = () => {
                         {isUploading ? 'Uploading...' : 'Upload'}
                     </button>
                     <button onClick={closeModal}>Close</button>
+                </div>
+            </Modal>
+            <Modal isOpen={isDeleteModalOpen} onRequestClose={closeModal} contentLabel="Confirm Deletion" style={modalStyles}>
+                <h2 style={{ fontSize: '20px', marginBottom: '20px' }}><b>Confirm Deletion</b></h2>
+                <p>Are you sure you want to delete this resource?</p>
+                <div>
+                    <button style={{ marginRight: '40px' }} onClick={handleDelete}>
+                        Confirm
+                    </button>
+                    <button onClick={closeModal}>Cancel</button>
                 </div>
             </Modal>
         </div>
