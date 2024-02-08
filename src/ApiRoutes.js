a8bc0dc40e84fa01319a5d7f2433eeb880c90cbd
ngrayzin <rayzin147@gmail.com>
2024-02-09 03:56:19 +0800

    changed url

--- src/ApiRoutes.js
+++ src/ApiRoutes.js
@@ -3,35 +3,35 @@ class ApiRoutes {
 
     authSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5010`
-        : "localhost:3010";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5010";
 
     resourceSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5011`
-        : "localhost:3011";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5011";
 
     searchSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5012`
-        : "localhost:3012";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5012";
 
     taskSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5013`
-        : "localhost:3013";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5013";
 
     projectSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5014`
-        : "localhost:3014";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5014";
 
     forumSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5015`
-        : "localhost:3015";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5015";
 
     organisationSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5016`
-        : "localhost:3016";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5016";
 
     notificationsSVC = process.env.NEXT_PUBLIC_PORT
         ? `${process.env.NEXT_PUBLIC_DOMAIN}:5017`
-        : "localhost:3017";
+        : "https://ec2-54-173-92-27.compute-1.amazonaws.com:5017";
 }
 
 export default ApiRoutes;
